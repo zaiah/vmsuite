9a37f4bd15049c0b70bcd760d556edfc5ba1c0fc
Antonio Collins <ramar.collins@gmail.com>
2013-11-05 07:29:11 -0500

    Got rid of pickIso() function.

--- vmbuilder.sh
+++ vmbuilder.sh
@@ -78,34 +78,6 @@ General Options:
 }
 
 
-# pickIso()
-# Select an ISO file (b/c they can be pretty hard to spell out)
-pick_iso() {
-	# Generate a short menu of ISO files.
-	# This will be coming from the databse
-	# disk_img table
-	printf "Please select the image you like to use for your new node:\n\n"
-	declare -a avail_imgs=($(find -L "$ISO_DIR" -iname "*.iso")) 
-
-	# Show a long menu.
-	for pcount in $(seq 0 ${#avail_imgs[*]})
-	do
-		if [ $pcount == ${#avail_imgs[*]} ]
-		then
-			break
-		fi	
-		index=$(( $pcount + 1 ))
-		printf "\t${index})${avail_imgs[${pcount}]}\n"
-	done
-
-	read ans
-	n=$(( $ans - 1 ))
-
-	# Should we have any reason to mount this?
-	iso=${avail_imgs[$n]}
-}
-
-
 # Die on no arg.
 [ -z $BASH_ARGV ] && printf "Nothing to do\n" && usage 1
 while [ $# -gt 0 ]
